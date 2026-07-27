4e7c2e598d947e530320caeff43f2c6ae1bdf91e
jiodg45 <jiodg45@163.com>
2026-07-27 17:30:48 +0800

    Initial Commit

---
+++ FileUploadPlus/FileUploadPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileUploadPlusApp.swift
+//  FileUploadPlus
+//
+//  Created by Backup on 2026/7/27.
+//
+
+import SwiftUI
+
+@main
+struct FileUploadPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
